d39c2d0dc77d538458c827b5ea65f20c38991be6
Priyatham Reddy <priyathamreddy147@gmail.com>
2022-06-18 22:02:27 +0530

    Update Logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,18 @@
 import "./navbar.css"
+import { useNavigate } from "react-router-dom"
 
 const Navbar = () => {
+    const navigate = useNavigate()
+
+    const handleLogoClick = () => navigate("/")
+
     return (
         <>
             <div className="navbar">
                 <div className="navContainer">
-                    <a href="/" className="logo">
+                    <span className="logo" onClick={handleLogoClick}>
                         InstaBooking
-                    </a>
+                    </span>
                     <div className="navItems">
                         <button className="navButton">Register</button>
                         <button className="navButton">Login</button>
